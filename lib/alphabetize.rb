87a6185b170ef6cfa662e6dd8610e03418b95a47
han xu <hanxu27@gmail.com>
2019-04-17 16:10:07 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,10 +6,10 @@ def alphabetize(arr)
   # code here
   
   arr.sort_by do |phrase|
-    phrase.split("").map do |character|
+    #phrase.split("").map do |character|
       #binding.pry
-      ESPERANTO_ALPHABET.index(character)
-      
+      #ESPERANTO_ALPHABET.index(character)
+      [4, 1, 5, 2]
     end
   end
 end
